0b45a117b1166c3c2566f387a07cbd0ac955b270
flipflop133 <francois.bechet@tutanota.com>
2025-04-19 09:54:04 +0200

    fix: update project name from energy_analyzer to energytrackr in configuration files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../../src"))
 
-project = "energy_analyzer"
+project = "energytrackr"
 copyright = "2025, François Bechet"
 author = "François Bechet"
 release = "0.1.0"
